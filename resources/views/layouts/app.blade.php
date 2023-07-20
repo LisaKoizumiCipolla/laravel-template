e15e5e0b39f4b898ee810dd650e512af81211dc1
LisaKoizumiCipolla <cipollalisa17@gmail.com>
2023-07-20 18:19:14 +0200

    Adjustments

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,10 +3,21 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    @yield('custom-stylesheets')
+    @yield('custom-script')
+    <title>@yield('title', 'App')</title>
 </head>
+
 <body>
     
+    @include('partials.header')
+
+    <main>
+        @yield('main-section')
+    </main>
+
+    @include('partials.footer')
+
 
 
     @vite('resources/js/app.js')
